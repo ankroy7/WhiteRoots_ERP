e052de75bb4225cec00266d941569d98627931ed
ankroy7 <52830086+ankroy7@users.noreply.github.com>
2026-04-25 12:34:30 +0530

    Create WhiteRootsEtpApplication.java as the main Spring Boot application class

---
+++ src/main/java/com/whiteroots/erp/WhiteRootsEtpApplication.java
@@ -0,0 +1,12 @@
+package com.whiteroots.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteRootsEtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteRootsEtpApplication.class, args);
+    }
+}
